4a87fbc85d96e696a8f083dbf1a97b42101d5037
adeshina bhadmus <adeshinabhadmus5@gmail.com>
2024-12-22 18:38:45 +0100

    adding comments to my code

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let quantNum=0
 let totalCart=0;
 
+//page items 
 const menu=[
   {
     quantity:0,
@@ -54,7 +55,7 @@ const menu=[
   },
 ]
 
-console.log(quantNum)
+// variable for accumulating cart
 let cartProduct=[];
 
 const wrapper=document.querySelector('.food-wrapper');
@@ -62,6 +63,8 @@ const searchbar=document.getElementById('searchbar');
 const searchBtn=document.querySelector('.search-btn');
 const foodbtns=document.querySelectorAll('.btn');
 
+
+// loading the webpage
 document.body.addEventListener('load',onLoad(menu))
 
 
@@ -100,6 +103,8 @@ document.body.addEventListener('load',onLoad(menu))
       wrapper.innerHTML=result
 
     }
+
+    // displaying the cart onclick 
        const cartIcon=document.querySelector('.cart-icon');
        const times=document.querySelector('.times');
        const overlay=document.querySelector('.overlay');
@@ -108,7 +113,7 @@ document.body.addEventListener('load',onLoad(menu))
        const EmptyMsg=document.querySelector('.empty-msg')
        cartIcon.addEventListener('click',()=>{
         cart.style.right='0%';
-        document.title="cart"
+        document.title="SweetFoods-Cart"
         times.style.display='block';
         overlay.style.display='block'; 
         if(totalCart===0){
@@ -119,11 +124,16 @@ document.body.addEventListener('load',onLoad(menu))
           EmptyMsg.style.display='none'
         }
        })
+      //  closing the cart onclick
        times.addEventListener('click',()=>{
         cart.style.right="-100%"
         times.style.display='none';
         overlay.style.display='none';
+        
+        document.title="SweetFoods-Home"
        })
+
+      //  each food item functionality
     function reuse(){
       const articles=document.querySelectorAll('.article');
       const cartQuantity=document.querySelector('.cart')
@@ -132,8 +142,8 @@ document.body.addEventListener('load',onLoad(menu))
         const show=article.querySelector('.show-add');
         let quantity=article.querySelector('.quantity') 
         const qtyBtn=article.querySelectorAll('.qty-btns');
-    
-    
+
+        // increasing and decreasing food item number
         qtyBtn.forEach((btn)=>{
           btn.addEventListener('click',()=>{
             const id=btn.id
@@ -148,12 +158,13 @@ document.body.addEventListener('load',onLoad(menu))
               quantity.innerHTML++
     
             }
-            
+   
           });
           
         })
         
         
+        // Adding food to cart
         const foodNum=document.querySelector('.food-num');
         const amount=document.querySelector('.amount')
         const tax=document.querySelector('.tax')
@@ -165,8 +176,6 @@ document.body.addEventListener('load',onLoad(menu))
           }else{
             show.innerHTML='Added Sucessfully'
            
-            
-           
             for(i=0;i<menu.length;i++){
               let item=menu[i];
 
@@ -176,9 +185,12 @@ document.body.addEventListener('load',onLoad(menu))
                 let numAmt=Number(amount.innerHTML)
                 let taxAmt=Number(tax.innerHTML)
                 let totalAmt=Number(total.innerHTML)
+                // increasing food item if it is already in the cart
                 if(cartProduct.includes(item)){
-                  item.quantity=quantity.innerHTML
+                  item.quantity=quantNum
                   addToCart();
+
+                  // calculating cost of foods after increasing quantity
                   foodNum.innerHTML='('+ cartProduct.length + ')'
                   numAmt+=qaunt*(item.price/100)
                   amount.innerHTML=numAmt.toFixed(1)
@@ -189,12 +201,14 @@ document.body.addEventListener('load',onLoad(menu))
                   total.innerHTML=totalAmt.toFixed(1)
                   
                 }else{
+                  // adding food to cart if it is not in cart and increasing total  foods in cart
                   totalCart++
                   cartQuantity.innerHTML=totalCart;
                   item.quantity=quantity.innerHTML;
                   cartProduct.push(item)
-                  console.log(cartProduct)
                   addToCart();
+
+                  // calculating cost of foods
                   foodNum.innerHTML='('+ cartProduct.length + ')'
                   numAmt+=qaunt*(item.price/100)
                   amount.innerHTML=numAmt.toFixed(1)
@@ -206,8 +220,8 @@ document.body.addEventListener('load',onLoad(menu))
                 }
 
                 
+                  // cart item functionality
                   const carts=document.querySelectorAll('.cart-product');
-                  console.log(carts)
                   carts.forEach((cart)=>{
                     const cartitem=cart.querySelectorAll('.cart-container');
                     cartitem.forEach((cartIt)=>{
@@ -217,6 +231,7 @@ document.body.addEventListener('load',onLoad(menu))
                       const cartBtn=cartIt.querySelectorAll('.cart-btns');
                       const delBtn=cartIt.querySelector('.addbtn');
                   
+                      // increasing cart food item quantity
                       cartBtn.forEach((btn)=>{ 
                         btn.addEventListener('click',()=>{
                           const id=btn.id 
@@ -233,32 +248,35 @@ document.body.addEventListener('load',onLoad(menu))
                           }
                         });
                   
-
+                        // updating the quantity on the webpage and total food amount
                       updateBtn.addEventListener('click',()=>{      
                         const update=Number(updateQty.innerHTML);
                         updateQty.innerHTML=cartQty.innerHTML 
                         const updatedQuant=Number(updateQty.innerHTML);
                         const sub=updatedQuant - update
+                       
+
+                        // calculating cost of foods after updating food quantity
                         const newprice=(item.price/100)*sub;
                         numAmt+=newprice
                         amount.innerHTML=numAmt.toFixed(1)
                         taxAmt=(numAmt * 0.1)
                         totaltax=taxAmt.toFixed(1)
+
                         tax.innerHTML=totaltax
                         totalAmt=numAmt+Number(totaltax)
                         total.innerHTML=totalAmt.toFixed(1)
                         if(update===0){
                           totalCart--
                           cartQuantity.innerHTML=totalCart;
-                        
                         }
                     
                       })  
                     })
+                    // deleting cart food item
                     delBtn.addEventListener('click',()=>{
-                      cartProduct.splice(i,1);
-                      addToCart();
                       const update=Number(updateQty.innerHTML);
+                      // calculating cost of foods after deleting a food item
                       const deletedAmt=(item.price/100)*update
                       numAmt-=deletedAmt
                       amount.innerHTML=numAmt.toFixed(1)
@@ -268,12 +286,13 @@ document.body.addEventListener('load',onLoad(menu))
                       totalAmt=numAmt+Number(totaltax)
                       total.innerHTML=totalAmt.toFixed(1)
                       
+                      // decreasing total item in cart after deleting an item
                       totalCart--
                       cartQuantity.innerHTML=totalCart;
 
                       if(totalCart===0){
-                                  EmptyMsg.style.display='grid'
-                            cartContent.style.display='none'
+                         EmptyMsg.style.display='grid'
+                         cartContent.style.display='none'
                       }
                     })
  
@@ -290,9 +309,13 @@ document.body.addEventListener('load',onLoad(menu))
     
     }
  
+
+    // search functionality
   const errorMessage=document.querySelector('.error-message');
   const message=document.querySelector('.message');
    
+    
+  // filtering menu array to search for food
     function search(){
       const searchval=searchbar.value.toLowerCase()
       const emptyArray=[]
@@ -305,19 +328,23 @@ document.body.addEventListener('load',onLoad(menu))
         }
         
       })
+      // showing search result on webpage
 
       document.body.addEventListener('load',onLoad(serchResult));
       reuse();
 
+      // search error message
       if(JSON.stringify(serchResult)=== JSON.stringify(emptyArray)){
         errorMessage.innerHTML=`<h1>No Result For "${searchval}"</h1>`
       }
     }
 
+    // clearing input field after running a search
     searchBtn.addEventListener('click',()=>{
        search();
        searchbar.value=''
     });
+    // running seaech function using enter key
     searchbar.addEventListener('keyup',(event)=>{
       if(event.key=== 'Enter'){
          search();
@@ -326,12 +353,13 @@ document.body.addEventListener('load',onLoad(menu))
     });
              
     
-
+    // Button Functionality
     foodbtns.forEach(function(btn){
       btn.addEventListener('click',()=>{
         message.innerHTML=''
         errorMessage.innerHTML=''
         let id=btn.dataset.id
+        // filtering menu to match category of foods
         result= menu.filter((value)=>{
           if(id === value.id){
             return value.id === id;
@@ -341,12 +369,15 @@ document.body.addEventListener('load',onLoad(menu))
             return value
           }
         });
+        
+        // display result on webpage
         document.addEventListener('load',onLoad(result)) 
         reuse();       
       });
     });
 
 
+    // loading cart item on the webpage
     function addToCart(){
       console.log(cartProduct)
       let cartHtml=''
@@ -377,7 +408,8 @@ document.body.addEventListener('load',onLoad(menu))
           <div class="delete-cont">
             <span class="price">$${value.price/100}</span>
             <button class="addbtn" onclick="
- 
+             cartProduct.splice(${index},1);
+             addToCart();
             "><i class="fa fa-trash"></i></button>
           </div>
        </article>
@@ -387,7 +419,6 @@ document.body.addEventListener('load',onLoad(menu))
         });
 
      document.querySelector('.cart-product').innerHTML=cartHtml; 
-     
     }
     reuse();
     
